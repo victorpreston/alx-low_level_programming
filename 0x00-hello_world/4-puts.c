89672bbdba08fee52f1aa724eaaada22a4c2fa64
victorpreston <prestonvictor25@gmail.com>
2023-06-15 04:15:49 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main (void)
+{
+	puts("Programming is like building a multilingual puzzle\n");
+	return (0);
+}
