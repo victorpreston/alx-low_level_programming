a272b0bc0676054e974b2493a9e9325bc34a6e1f
victorpreston <prestonvictor25@gmail.com>
2023-06-15 04:23:59 -0700

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,8 +4,8 @@
  *
  * Return: Always 0 (success)
  */
-int main (void)
+int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
