e95dd20187d37390b03b3c181ea9fd8758fb62a3
victorpreston <prestonvictor25@gmail.com>
2023-07-28 16:26:05 -0700

    Update Code

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,42 +1,39 @@
 #include "lists.h"
 
-/**START**/
-
 /**
- * print_list - printing elements of a list
+ * ****** ALX PROJECTS******
  *
- * @h: pointer: shows to head first node
+ * print_list - prints elements of linked-list
  *
- * Return: Shows the No of nodes
+ * @h: pointer -shows to head first node
  *
- * ALX Projects.
+ * Return: The No of nodes counted
 */
 
 size_t print_list(const list_t *h)
 {
-	/*pointer approach*/
-	const list_t *pointer = h;
-	size_t node_count = 0;
-	/*nodes counted*/
-	if (pointer == NULL)
+	size_t count_nod = 1;
+
+	/*nodes*/
+	if (h == NULL)
 		return (0);
 
-	while (pointer->next != NULL)
-	{
-		/*I will get this right*/
-		if (pointer->str == NULL)
-			printf("[%d] %s\n", 0, "(nil)");
-		else
-			printf("[%d] %s\n", pointer->len, pointer->str);
+	do {
+		switch (h->next == NULL) {
+			case 0:
+				if (h->str == NULL)
+					printf("[%d] %s\n", 0, "(nil)");
+				else
+					printf("[%d] %s\n", h->len, h->str);
 
-		/*pass_node*/
-		pointer = pointer->next;
-		/*node_count*/
-		count_nod += 1;
-	}
-	/************last_node*********/
-	printf("[%d] %s\n", pointer->len, pointer->str);
+				h = h->next;
+				count_nod += 1;
+				break;
+			default:
+				printf("[%d] %s\n", h->len, h->str);
+				count_nod += 1;
+		}
+	} while (h->next != NULL);
 
 	return (count_nod);
 }
-/**STOP**/
