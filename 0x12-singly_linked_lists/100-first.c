fccae9349cc3ea5ccfb6fddf374eccb7d9ba07eb
victorpreston <prestonvictor25@gmail.com>
2023-07-28 19:31:32 -0700

    premain

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -9,10 +9,14 @@
  *
  */
 
-void __attribute__((constractor)) premain_func()
+void premain_func(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
 }
-
+void premain(void) __attribute__((constructor))
+void premain(void)
+{
+	premain_func();
+}
 /*****************************STOP***************************/
