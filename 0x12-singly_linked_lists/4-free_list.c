97f582824f737f8dab165b9bde92dc7b99eee1e6
victorpreston <prestonvictor25@gmail.com>
2023-07-28 18:57:26 -0700

    Update Code

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/*****************************START******************************/
+/**
+ * free_list - This function will free a list
+ * @head: pointer to the list head
+ *
+ * ALX PROJECTS
+ */
+
+void free_list(list_t *head)
+{
+	list_t now;
+
+	do {
+		now = head->next;
+		free(head->str);
+		free(head);
+
+		head = now;
+	} while (head != NULL);
+}
+/****************************STOP*******************************/
