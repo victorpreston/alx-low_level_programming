1c457ccfc1fe280953c7a4a9d336b3413c49e692
victorpreston <prestonvictor25@gmail.com>
2023-07-28 18:01:04 -0700

    Update Code

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,40 @@
+#include "lists.h"
+
+/******************START*************************/
+/**
+ * add_node - adds a nod at start  of a list
+ *@head: shows to the head of the list
+ *@str: string to be added as node to list
+ *
+ * Return:new element/ NULL if element fails
+ * ALX PROJECTS
+*/
+list_t *add_node(list_t **head, const char *str)
+{
+	size_t len = 0;
+	list_t *added;
+
+	for (len = 0; str != NULL && str[len] != '\0'; len++)
+	{
+		/**********implement len untill NULL- terminal is reached*****/
+	}
+
+	added = malloc(sizeof(list_t));
+	if (added == NULL)
+		return (NULL);
+	/**I will initialize and find Null if head is NUll**/
+	if (*head == NULL)
+		added->next = NULL;
+	else
+		added->next = *head;
+
+	/**Now I will try to add it**/
+	added->str = strdup(str);
+	added->len = len;
+	*head = added;
+
+	/**return *head **/
+
+	return (*head);
+}
+/*********************STOP******************/
