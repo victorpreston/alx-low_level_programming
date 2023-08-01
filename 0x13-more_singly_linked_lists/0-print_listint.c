8c624e903fa102a7fa167938790eca3165350d63
victorpreston <prestonvictor25@gmail.com>
2023-08-01 14:21:56 -0700

    Update Code

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -5,7 +5,7 @@
 /**
  * print_listint - printsall the elements in a linked list
  *
- * @h: pointer: shows the first node
+ * @h: pointer: shows the first node.
  *
  * Return: returns the No of nodeds in the list_t
  */
@@ -14,13 +14,16 @@ size_t print_listint(const listint_t *h)
 	const listint_t *pointer = h;
 	size_t nod = 0;
 
+	if (pointer == NULL) //empty list
+		return (0);
+
 	/***Do.....While loop****/
 	do {
 		nod++;
 		printf("%d\n", pointer->n);
 		pointer = pointer->next;
-	} while (pointer);
+	} while (pointer != NULL);
 
 	return (nod);
 }
-/**************STOP***************/
+/**************STOP*****************/
