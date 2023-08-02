5c3675a3418de6d6d97da45c5a61689f942e8f74
victorpreston <prestonvictor25@gmail.com>
2023-08-01 19:26:48 -0700

    Update Lists.h

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -28,6 +28,11 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
 void free_listint(listint_t *head);
 void free_listint2(listint_t **head);
 int pop_listint(listint_t **head);
+int delete_nodeint_at_index(listint_t **head, unsigned int index);
+listint_t *reverse_listint(listint_t **head);
+size_t print_listint_safe(const listint_t *head);
+size_t free_listint_safe(listint_t **h);
+listint_t *find_listint_loop(listint_t *head);
 
 #endif
 /***************HEADER***************/
