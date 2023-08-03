b29363b133f6bac74431e83ee0ff9ac3be1e662c
victorpreston <prestonvictor25@gmail.com>
2023-08-03 14:56:50 -0700

    3-set_bit.c

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+
+
+
+/*******************START*******************/
+/**
+ * set_bit - function: sets bit in position to one (1).
+ * @n:pointer: shows at the bit.
+ * @index:shows the index to be set.
+ *
+ * Return: On success, 1 is returned
+ *         Elsewhere we get -1 when on failure.
+ *
+ *
+ *
+ * ALX PROJECTS
+ */
+
+
+
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	/*set new constants to work with*/
+	/**
+	 * number = n.
+	 * tag = index.
+	 */
+	unsigned long int *number = n;
+	unsigned int tag = index;
+
+	if (number == NULL || tag >= (sizeof(unsigned long int) * 8))
+		return (-1);
+
+	unsigned long int mask = 1UL << tag;
+	*number = *number | mask;
+
+	return (1);
+}
+/******************STOP**********************/
