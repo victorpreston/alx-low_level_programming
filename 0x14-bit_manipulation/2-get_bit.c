583e6b419e4dc4393950cf99de6ab8d8cc45ae1b
victorpreston <prestonvictor25@gmail.com>
2023-08-03 14:33:57 -0700

    2-get_bit.c

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+
+
+/******************START*****************/
+/**
+ * get_bit - func:finds value of bit at an index.
+ * @n:bit: the bit itself.
+ * @index: index, all indices start at zero(0).
+ *
+ * Return: for if  an error occurs
+ *         Elsewhre bit at index.
+ *
+ *
+ * ALX PROJECTS
+ */
+
+
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	/**set new constants**/
+	unsigned long int number = n;
+	unsigned int tag = index;
+
+	/******Check if index is out of range*****/
+	if (tag >= (sizeof(unsigned long int) * 8))
+		return (-1);
+	/*Error, Index out of range*/
+
+	unsigned long int mask = 1UL << tag;
+	return ((number & mask) ? 1 : 0);
+}
+/******************STOP*******************/
