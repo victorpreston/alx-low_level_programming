1628bb4d37651a85456bde2f3ae86e694d7f8b81
victorpreston <prestonvictor25@gmail.com>
2023-08-03 16:05:21 -0700

    5-flip_bits.c

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/******************START********************/
+/**
+ * flip_bits - function: counts No of Bits
+ *             flipped from No1 to No2 or another.
+ * @n:pointer: number to be flipped ny the function
+ * @m:flipping number or constant.
+ *
+ * Return:flipped bits n ^ m
+ *
+ *
+ * ALX PROJECTS
+ */
+
+
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int no_flp = n^m;
+	int num_ct = 0;
+
+	/*loop*/
+	for (; no_flp; no_flp >>= 1)
+	{
+		if (no_flp & 1)
+			num_ct++;
+	}
+
+	return (num_ct);
+}
+/**********************STOP******************/
