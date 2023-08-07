856165de7776afa18cd7ed6e86e7b7558ad30615
victorpreston <prestonvictor25@gmail.com>
2023-08-07 04:58:09 -0700

    main.h

---
+++ 0x15-file_io/main.h
@@ -0,0 +1,19 @@
+#ifndef H_MAIN_
+#define H_MAIN_
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <elf.h>
+
+
+ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
+
+
+#endif
