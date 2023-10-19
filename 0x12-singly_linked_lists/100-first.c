d35c3812f26746c9d0f0e5c3a922a3f648708e81
victorpreston <prestonvictor25@gmail.com>
2023-10-19 09:51:27 -0700

    100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,22 +1,14 @@
-#include "lists.h"
 #include <stdio.h>
 
-/**
- * premain_func - function that runs befor the main
- *
- *
- * Return: Does not return anything for this code
- *
- */
+void __attribute__((constructor)) hare(void);
 
-void premain_func(void)
+/**
+ * hare - print.
+ *
+ *Description: print.
+ */
+void hare(void)
 {
-	printf("You're beat! and yet, you must allow,\n");
-	printf("I bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n"
+	       "I bore my house upon my back!\n");
 }
-void premain(void) __attribute__((constructor));
-void premain(void)
-{
-	premain_func();
-}
-/*****************************STOP***************************/
