739ef0e18f9728a5f532e3e3f4262ad7cb9a750a
victorpreston <prestonvictor25@gmail.com>
2023-10-19 11:19:01 -0700

    4-free_listint.c

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -1,26 +1,20 @@
 #include "lists.h"
 
-
-/***********START*********/
 /**
- * free_listint - function shows a free list
- *
- * @head:pointer: -shows node_one of the list
- *
- * Return: Always 0
- *
- * ALX PROJECTS
- */
+*free_listint - frees a linked list
+*@head: head of the list
+*
+*Return: void
+*/
 
 void free_listint(listint_t *head)
 {
-	listint_t *nod_c;
-	/**Loop**/
+listint_t *temp;
 
-	do {
-		nod_c = head->next;
-		free(head);
-		head = nod_c;
-	} while (head != NULL);
+while (head != NULL)
+{
+temp = head;
+head = head->next;
+free(temp);
 }
-/***********STOP***********/
+}
