307a7d14029b78f69e1e1ef63e1d6bae769d127f
victorpreston <prestonvictor25@gmail.com>
2023-12-07 08:09:43 -0800

    102-result

---
+++ 0x17-doubly_linked_lists/result.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def is_palindrome(num):
+    # Check if the number is a palindrome
+    return str(num) == str(num)[::-1]
+
+largest_palindrome = 0
+
+for i in range(100, 1000):
+    for j in range(i, 1000):  # Start from i to avoid duplicate combinations
+        product = i * j
+        if is_palindrome(product) and product > largest_palindrome and 99 < product < 1000:
+            largest_palindrome = product
+
+# Save the result to the file
+with open('102-result', 'w') as file:
+    file.write(str(largest_palindrome))
