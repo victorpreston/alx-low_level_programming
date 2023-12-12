640a4b76744a0263a9ad49f6598cd89017168c4a
victorpreston <prestonvictor25@gmail.com>
2023-12-12 03:45:56 -0800

    101-make_me_win.sh

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P .. https://raw.githubusercontent.com/monoprosito/holbertonschool-low_level_programming/master/0x18-dynamic_libraries/libgiga.so
-export LD_PRELOAD="$PWD/../libgiga.so"
+wget -P /tmp https://github.com/victorpreston/alx-low_level_programming/raw/master/0x18-dynamic_libraries/putshack.so
+export LD_PRELOAD=/tmp/putshack.so
